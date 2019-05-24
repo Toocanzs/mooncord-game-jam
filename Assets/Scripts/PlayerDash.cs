206d2915cd336cb90e8c496f6162c030ff07b403
Toocanzs <dupanzszs@gmail.com>
2019-05-24 15:56:25 -0400

    add collision

---
+++ Assets/Scripts/PlayerDash.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerDash : MonoBehaviour
+{
+    [SerializeField]
+    float maxDashDistance = 1f;
+    [SerializeField]
+    float dashSpeed = 5f;
+    private PlayerMovement playerMovement;
+
+    float dashDistanceTraveled = 0f;
+    bool dashing = false;
+    float2 dashDirection = new float2(0,1);
+
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+    void Update()
+    {
+        float2 input = new float2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (math.dot(input, input) != 0f)
+        {
+            dashDirection = math.normalize(input);
+        }
+        if (Input.GetButtonDown("Jump") && !dashing)
+        {
+                dashing = true;
+                playerMovement.enabled = false;
+        }
+        if(dashing)
+        {
+            float startDist = dashDistanceTraveled;
+            dashDistanceTraveled = math.min(dashDistanceTraveled + Time.deltaTime * dashSpeed, maxDashDistance);
+            float delta = dashDistanceTraveled - startDist;
+            transform.position += new Vector3(dashDirection.x, dashDirection.y, 0) * delta;
+
+            if (dashDistanceTraveled >= maxDashDistance)
+            {
+                dashing = false;
+                playerMovement.enabled = true;
+                dashDistanceTraveled = 0f;
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,20 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Mathematics;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed = 1f;
+    [SerializeField]
+    private LayerMask layerMask;
+
+    private BoxCollider2D boxCollider;
     void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
         float2 input = new float2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         if (math.dot(input, input) != 0f)
+        {
             input = math.normalize(input);
-        transform.position += (Vector3)new float3(input * Time.deltaTime * moveSpeed, 0);
+            float movementDelta = Time.deltaTime * moveSpeed;
+            RaycastHit2D hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, input, movementDelta, layerMask);
+            Debug.Log("norm: " + hit.normal);
+            if(hit.transform == null)
+            {
+                transform.position += (Vector3)new float3(input * movementDelta, 0);
+            }
+            else
+            {
+                transform.position += (Vector3)new float3(input * movementDelta * hit.fraction, 0);
+            }
+        }
     }
 }
