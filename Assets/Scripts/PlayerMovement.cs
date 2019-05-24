5f0620525d45475f732a4417beab6f5592ef1699
Toocanzs <dupanzszs@gmail.com>
2019-05-24 13:18:08 -0400

    Add view cone script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 1f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float2 input = new float2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (math.dot(input, input) != 0f)
+            input = math.normalize(input);
+        transform.position += (Vector3)new float3(input * Time.deltaTime * moveSpeed, 0);
+    }
+}
---
+++ Assets/Scripts/ViewCone.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[DefaultExecutionOrder(10)]
+[RequireComponent(typeof(Camera))]
+public class ViewCone : MonoBehaviour
+{
+    new private Camera camera;
+    [HideInInspector]
+    public RenderTexture colorRenderTexture;
+    [HideInInspector]
+    public RenderTexture wallMask;
+
+    [SerializeField]
+    private ComputeShader computeShader;
+    private int mainKernel;
+
+    void Start()
+    {
+        //TODO: Change these buffers when resolution changes etc.
+        camera = GetComponent<Camera>();
+        colorRenderTexture = new RenderTexture(camera.pixelWidth, camera.pixelHeight, 0,
+            RenderTextureFormat.Default, RenderTextureReadWrite.Default);
+        wallMask = new RenderTexture(camera.pixelWidth, camera.pixelHeight, 0,
+            RenderTextureFormat.R8, RenderTextureReadWrite.Default);
+        colorRenderTexture.Create();
+        RenderBuffer[] renderBuffers = new RenderBuffer[] { colorRenderTexture.colorBuffer, wallMask.colorBuffer };
+        camera.SetTargetBuffers(renderBuffers, colorRenderTexture.depthBuffer);
+
+        mainKernel = computeShader.FindKernel("Main");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(colorRenderTexture, destination);
+    }
+}
