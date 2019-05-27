e51e846873688bec63c8310ad4f98ce2f9f42d92
Toocanzs <dupanzszs@gmail.com>
2019-05-26 22:46:24 -0400

    add vision blocking attack

---
+++ Assets/VisionBlock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionBlock : MonoBehaviour
+{
+    [SerializeField]
+    private AnimationCurve animationCurve;
+    private float time = 0f;
+    [SerializeField]
+    private float maxTime = 0f;
+    [SerializeField]
+    private float maxHeight = 5f;
+
+    private Vector3 startSize;
+
+    void OnEnable()
+    {
+        time = 0f;
+        startSize = transform.localScale;
+    }
+    void Update()
+    {
+        float t = time / maxTime;
+        if (t > 1f)
+        {
+            //Destroy(this);
+            return;
+        }
+        transform.localScale = new Vector3(startSize.x, animationCurve.Evaluate(t) * maxHeight, startSize.z);
+        time += Time.deltaTime;
+    }
+}
